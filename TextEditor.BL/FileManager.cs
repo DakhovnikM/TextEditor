0bf9b67d88433ae1c4d8e6f97d6163b19fcbe1da
Maxim_D <dahovnik@gmail.com>
2020-12-16 22:27:41 +0200

    Добавлены методы записи строки в файл и метод подсчета символов в строке.

--- TextEditor.BL/FileManager.cs
+++ TextEditor.BL/FileManager.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 
 namespace TextEditor.BL
@@ -15,10 +16,26 @@ namespace TextEditor.BL
 
         public string GetString(string filePath, Encoding encoding)
         {
-            var content = File.ReadAllText(filePath, encoding);
+            var str = File.ReadAllText(filePath, encoding);
 
-            return content;
+            return str;
         }
 
+        public void SaveString(string filePath, string content)
+        {
+            SaveString(filePath, content, defaultEncoding);
+        }
+
+        public void SaveString(string filePath, string content, Encoding encoding)
+        {
+            if (File.Exists(filePath))
+                File.WriteAllText(filePath, content, encoding);
+
+        }
+
+        public int GetSimbolCount(string count)
+        {
+            return count.Length;
+        }
     }
 }
